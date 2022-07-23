3de6fed6731af6eecedec672300abb3ae7e0f134
Limitput <109011938+Limitput@users.noreply.github.com>
2022-07-23 21:48:38 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	printf("请输入一个数");
+//	scanf("%d", &m);
+//	if (m % 5 == 0)
+//		printf("YSE\n");
+//	else
+//		printf("NO\n");
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	scanf("%d", & a);
+	if (a % 2 == 0)
+		printf("%d\n", a);
+	else
+		printf("no");
+
+	return 0;
+}
